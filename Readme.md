071b733b054b2378ad49f8ec2774ebf8e6846de6
廖政 <813144314@qq.com>
2021-09-10 00:15:03 +0800

    只出现一次的数字

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This is based algorithm folder.
+It is from url:https://leetcode-cn.com/leetbook/detail/top-interview-questions-easy/
--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-console.time('global')
+console.time("global");
 //删除排序数组中的重复项
 // var removeDuplicates = function (nums) {
 //     var acc;
@@ -40,31 +40,59 @@ console.time('global')
 // rotate(ins,5);
 // console.log(ins);
 
-//存在重复元素
+//找出存在的重复元素
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {
-    if (nums.length <= 1) {
-        return false;
-    }
-    let max = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        if (max < nums[i]) {
-            max = nums[i]
-        } else if (max == nums) {
-            return true;
-        } else {
-            for (let j = 0; j < i; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
-            }
-        }
-    }
-};
-let ins = [1,2,3,4,5,6,7,8,9,11,9];
-let out = containsDuplicate(ins);
-console.log(out);
-console.timeEnd('global')
+// var containsDuplicate = function (nums) {
+//   if (nums.length <= 1) {
+//     return false;
+//   }
+//   let max = nums[0];
+//   for (let i = 1; i < nums.length; i++) {
+//     if (max < nums[i]) {
+//       max = nums[i];
+//     } else if (max == nums) {
+//       return true;
+//     } else {
+//       for (let j = 0; j < i; j++) {
+//         if (nums[i] == nums[j]) {
+//           return true;
+//         }
+//       }
+//     }
+//   }
+// };   //这个方法在执行[1,2,3,4]时会报错；
+// let containsDuplicate = function (nums) {
+//   nums.sort((a,b)=>{return b-a});
+//   console.log(nums);
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == nums[i + 1]) {
+//       return true;
+//     }
+//   }
+// };
+// let containsDuplicate = function (nums) {
+//   let arySet = Array.from(new Set(nums));
+//   console.log(arySet);
+//   return arySet.length < nums.length;
+// };
+// let ins = [1, 2, 3, 2, 4, 5, 6, 7, 9, 11, 9];
+// let ins =[1,2,3,4];
+// let out = containsDuplicate(ins);
+// console.log(out);
+
+//只出现一次的数字
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//  var singleNumber = function(nums) {
+//     return nums.reduce((pre,cur)=>{
+//         return pre^cur
+//     })
+//  };
+// let nums = [2,2,1,1,3,3,4,5,5,6,7,8,8,7,6]
+// console.log(singleNumber(nums));
+console.timeEnd("global");
