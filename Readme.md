ca8b54a9ad0d48321710a46da69396a2af2fb604
廖政 <813144314@qq.com>
2021-09-13 13:02:26 +0800

    feat: add pulsOne arithmetic

--- Readme.md
+++ Readme.md
@@ -1,4 +1,8 @@
 This is based algorithm folder.
-It is from url:https://leetcode-cn.com/leetbook/detail/top-interview-questions-easy/
 
 draft.js is only a blank js file just for test.
+
+It is from 
+
+[力扣]: https://leetcode-cn.com/leetbook/detail/top-interview-questions-easy/
+
--- draft.js
+++ draft.js
@@ -1,27 +1,22 @@
-let nums1 = [61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81]
-let nums2 = [5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48];
+let nums = [9,1,2,3];
 
-var intersect = function (nums1, nums2) {
-    nums1.sort((a,b)=>a-b);
-    nums2.sort((a,b)=>a-b);
-    console.log(nums1,nums2);
-    let len1 = nums1.length;
-    let len2 = nums2.length;
-    let i = 0, j = 0;
-    let ary = [];
-    while (i < len1 && j < len2) {
-        if (nums1[i] < nums2[j]) {
-            i++;
-        } else if (nums1[i] == nums2[j]) {
-            ary.push(nums1[i]);
-            i++;
-            j++;
+var plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        // console.log(i)
+        // if (i == -1) {
+        //     digits.unshift(1);
+        //     return digits;
+        // }
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
         } else {
-            j++;
+            digits[i] = 0;
         }
-    };
-    return ary;
-}
-let res = intersect(nums1,nums2);
+    }
+    digits.unshift(1);
+    return digits;
+};
+let res = plusOne(nums);
 
 console.log(res);
